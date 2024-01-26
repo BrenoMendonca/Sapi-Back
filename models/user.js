a3f28efb7d17ac470810b6c5b0550feb5eaae337
Breno Mendonca <breno_a99@hotmail.com>
2024-01-25 21:41:46 -0300

    Fazendo melhorias no modelo de usuário

--- models/user.js
+++ models/user.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 
 const User = mongoose.model('User',{
-  name:String,
-  email:String,
-  password:String,
-  confirmpassword:String,
-  matricula:String,
+  name:{type: String,
+        required:true
+  },
+  email:{type: String,
+    required:true
+  },
+  password:{type: String,
+    required:true
+  },
+  confirmpassword:{type: String,
+    required:true
+  },
+  matricula:{type: String,
+    required:true
+  },
+  cpf:{type: String,
+    required:true
+  },
 })
 //Adicionar validação numerica na String de matricula
 module.exports = User
