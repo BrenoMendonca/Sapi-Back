916fa69c973abc41e27bc7b204040366580fc44b
ricardoddev <ricardodelgadofilhodev@gmail.com>
2024-05-03 14:31:28 -0300

    finalizando rota de adicionar/remover prof avaliador

--- controllers/Edital.js
+++ controllers/Edital.js
@@ -35,13 +35,13 @@ router.post('/add-prof-avaliador/:id', async (req, res) => {
         // Verifique se o edital existe
         const edital = await Edital.findById(id);
         if (!edital) {
-            return res.status(404).json({ msg: 'Edital não encontrado' });
+            return res.status(404).json({ msg: 'Edital não encontrado.' });
         }
 
         // Verifique se há matrículas duplicadas no array
         const matriculasUnicas = [...new Set(matriculas)];
         if (matriculasUnicas.length !== matriculas.length) {
-            return res.status(400).json({ msg: 'O array de matrículas contém matrículas duplicadas' });
+            return res.status(400).json({ msg: 'Contém matrículas duplicadas. Tente novamente.' });
         }
 
         // Verifique se todas as matrículas são válidas e não estão presentes como professores avaliadores
@@ -50,9 +50,9 @@ router.post('/add-prof-avaliador/:id', async (req, res) => {
         for (let matricula of matriculasUnicas) {
             const user = await User.findOne({ matricula });
             if (!user) {
-                erros.push(`Usuário com a matrícula ${matricula} não encontrado ou não é um professor`);
+                erros.push(`Usuário com a matrícula ${matricula} não encontrado ou não é um professor.`);
             } else if (edital.profsAvaliadores.some(avaliador => avaliador.matricula === user.matricula)) {
-                erros.push(`Usuário com a matrícula ${matricula} já é um professor avaliador deste edital`);
+                erros.push(`Usuário com a matrícula ${matricula} já é um professor avaliador deste edital.`);
             } else {
                 professoresAvaliadores.push({
                     id: user._id,
@@ -63,16 +63,42 @@ router.post('/add-prof-avaliador/:id', async (req, res) => {
         }
 
         if (erros.length > 0) {
-            return res.status(400).json({ msg: 'Alguns professores não puderam ser adicionados como avaliadores', erros });
+            return res.status(400).json({ msg: 'Alguns professores não puderam ser adicionados como avaliadores.', erros });
         }
 
         // Adicione os professores avaliadores ao edital
         edital.profsAvaliadores.push(...professoresAvaliadores);
         await edital.save();
 
-        res.status(200).json({ msg: 'Todos os professores avaliadores foram adicionados ao edital com sucesso' });
+        res.status(201).json({ msg: 'Todos os professores avaliadores foram adicionados ao edital com sucesso.' });
     } catch(error) {
         console.error('Erro ao adicionar professores avaliadores:', error);
+        res.status(500).json({ msg: 'Erro interno do servidor.' });
+    }
+})
+
+router.delete('/remove-prof-avaliador/:id', async (req,res) => {
+    try {
+        const { id } = req.params
+        const { matricula } = req.body
+
+        const edital = await Edital.findById(id)
+        if (!edital) {
+            return res.status(404).json({ msg: 'Edital não encontrado.' });
+        }
+        
+        const index = edital.profsAvaliadores.findIndex(avaliador => avaliador.matricula === matricula);
+
+        if (index === -1) {
+            return res.status(404).json({ msg: 'Professor avaliador não encontrado neste edital' });
+        }
+
+        edital.profsAvaliadores.splice(index, 1);
+        await edital.save();
+
+        return res.status(200).json({ msg: `Professor de matrícula ${matricula} retirado como avaliador` })
+    } catch (error) {
+        console.error('Erro ao remover professor avaliador:', error);
         res.status(500).json({ msg: 'Erro interno do servidor' });
     }
 })
