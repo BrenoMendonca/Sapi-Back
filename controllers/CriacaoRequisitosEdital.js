1977a87de385067832ca8980936fd3ecd0685f3f
Breno Mendonça <breno_a99@hotmail.com>
2024-12-28 18:16:07 -0300

    Organização de arquivos da API

---
+++ controllers/CriacaoRequisitosEdital.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const RequisitoPackage = require('../models/requisitosEdital');
+const router = express.Router();
+
+// ROTA CRIAÇÃO REQUISITOS EDITAL
+router.post("/criacao", async (req, res) => {
+    const { nameRequisito, requisitos } = req.body;
+
+    // Validações de campos obrigatórios
+    if (!nameRequisito || !requisitos) {
+        return res.status(422).json({ msg: 'Campo(s) não informados' });
+    }
+
+    // Validação de dados repetidos
+    const [requisitoByName] = await Promise.all([
+        RequisitoPackage.findOne({ nameRequisito: nameRequisito }) // Alterei para usar RequisitoPackage
+    ]);
+
+    const requisitoExists = requisitoByName;
+    if (requisitoExists) {
+        return res.status(422).json({ msg: 'Requisito de edital já cadastrado com esse nome' });
+    }
+
+    // Criação do novo pacote de requisitos
+    const novoRequisitoPackage = new RequisitoPackage({
+        nameRequisito,
+        requisitos
+    });
+
+    try {
+        // Salvando o novo pacote de requisitos
+        await novoRequisitoPackage.save();
+        return res.status(200).json({ msg: 'Requisito de edital salvo com sucesso!' });
+    } catch (error) {
+        console.error('Erro ao salvar requisito:', error);
+        return res.status(500).json({ msg: 'Erro interno do servidor' });
+    }
+});
